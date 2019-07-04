c94fed8ba64a373f4b6f9fa73d6c2d28a6d262c8
ScriptHash <52527330+ScriptHash@users.noreply.github.com>
2019-07-04 10:29:36 +0200

    Add files via upload

---
+++ apps.ps1
@@ -0,0 +1,69 @@
+# ---------------------------------------------------------------------------------------------------------------------------------------- #
+#															Deinstallation																   #
+# 							Befehl																				Beschreibung			   #							
+# ---------------------------------------------------------------------------------------------------------------------------------------- #
+Get-AppxPackage "*3dbuilder*"  | Remove-AppxPackage  -ErrorAction SilentlyContinue								# 3D Builder
+Get-AppxPackage "*3dviewer*"  | Remove-AppxPackage -ErrorAction SilentlyContinue								# 3D Viewer
+Get-AppxPackage "*Appconnector*"  | Remove-AppxPackage  -ErrorAction SilentlyContinue							# App Connector
+Get-AppxPackage "*Asphalt8Airborne*"  | Remove-AppxPackage  -ErrorAction SilentlyContinue						# Game Asphalt 8 Airborne
+Get-AppxPackage "*BethesdaSoftworks.FalloutShelter*"  | Remove-AppxPackage  -ErrorAction SilentlyContinue		# Game BethesdaSoftworks FalloutShelter
+Get-AppxPackage "*bingfinance*"  | Remove-AppxPackage  -ErrorAction SilentlyContinue							# Bing Finanzen
+Get-AppxPackage "*bingnews*"  | Remove-AppxPackage  -ErrorAction SilentlyContinue								# Bing News
+Get-AppxPackage "*bingsports*"  | Remove-AppxPackage  -ErrorAction SilentlyContinue								# Bing Sports
+Get-AppxPackage "*bingweather*"  | Remove-AppxPackage  -ErrorAction SilentlyContinue							# Bing Wetter
+Get-AppxPackage "*CandyCrushSodaSaga*"  | Remove-AppxPackage  -ErrorAction SilentlyContinue						# Game Candy Crush
+Get-AppxPackage "*CommsPhone*"  | Remove-AppxPackage  -ErrorAction SilentlyContinue								# CommsPhone
+Get-AppxPackage "*ConnectivityStore*"  | Remove-AppxPackage  -ErrorAction SilentlyContinue						# ConnectivityStore
+Get-AppxPackage "*DesktopAppInstaller*"  | Remove-AppxPackage -ErrorAction SilentlyContinue						# Desktop App Installer
+Get-AppxPackage "*DrawboardPDF*"  | Remove-AppxPackage  -ErrorAction SilentlyContinue							# Drawboard PDF	
+Get-AppxPackage "*Facebook*"  | Remove-AppxPackage  -ErrorAction SilentlyContinue								# Facebook App
+Get-AppxPackage "*FarmVille2CountryEscape*"  | Remove-AppxPackage  -ErrorAction SilentlyContinue				# Game Farmville
+Get-AppxPackage "*GetHelp*"  | Remove-AppxPackage  -ErrorAction SilentlyContinue								# GetHelp
+Get-AppxPackage "*getstarted*"  | Remove-AppxPackage  -ErrorAction SilentlyContinue								# GetStarted
+Get-AppxPackage "*Messaging*"  | Remove-AppxPackage  -ErrorAction SilentlyContinue								# Messaging
+Get-AppxPackage "*MinecraftUWP*"  | Remove-AppxPackage  -ErrorAction SilentlyContinue							# Game Minecraft
+Get-AppxPackage "*mobilfunktarif*"  | Remove-AppxPackage -ErrorAction SilentlyContinue							# App Mobilfunktarife
+Get-AppxPackage "*MSPaint*"  | Remove-AppxPackage  -ErrorAction SilentlyContinue								# MSPaint
+Get-AppxPackage "*Netflix*"  | Remove-AppxPackage  -ErrorAction SilentlyContinue								# App Netflix
+Get-AppxPackage "*Office.Sway*"  | Remove-AppxPackage  -ErrorAction SilentlyContinue							# Office Sway
+Get-AppxPackage "*officehub*"  | Remove-AppxPackage  -ErrorAction SilentlyContinue								# Office Hub
+Get-AppxPackage "*OneConnect*"  | Remove-AppxPackage  -ErrorAction SilentlyContinue								# One Connect
+Get-AppxPackage "*onenote*"  | Remove-AppxPackage  -ErrorAction SilentlyContinue								# vorinst. OneNote
+Get-AppxPackage "*PandoraMediaInc*"  | Remove-AppxPackage  -ErrorAction SilentlyContinue						# PandoraMedia
+Get-AppxPackage "*people*"  | Remove-AppxPackage  -ErrorAction SilentlyContinue									# People
+Get-AppxPackage "*SkypeApp*"  | Remove-AppxPackage  -ErrorAction SilentlyContinue								# vorinst. Skype
+Get-AppxPackage "*solitairecollection*"  | Remove-AppxPackage  -ErrorAction SilentlyContinue					# Game Solitaire
+Get-AppxPackage "*soundrecorder*"  | Remove-AppxPackage  -ErrorAction SilentlyContinue							# Soundrecorder
+Get-AppxPackage "*Twitter*"  | Remove-AppxPackage  -ErrorAction SilentlyContinue								# App Twitter
+Get-AppxPackage "*Microsoft.Todos*" | Remove-AppxPackage -ErrorAction SilentlyContinue							# Microsoft To-Do
+Get-AppxPackage "*Wallet*"  | Remove-AppxPackage  -ErrorAction SilentlyContinue									# Microsoft Wallet (neu Microsoft Pay)
+Get-AppxPackage "*Whiteboard*" | Remove-AppxPackage -ErrorAction SilentlyContinue								# Windows Whiteboard	
+Get-AppxPackage "*windowsalarms*"  | Remove-AppxPackage  -ErrorAction SilentlyContinue							# Windows Alarms
+Get-AppxPackage "*windowscamera*"  | Remove-AppxPackage  -ErrorAction SilentlyContinue							# Windows Camera
+Get-AppxPackage "*windowscommunicationsapps*"  | Remove-AppxPackage  -ErrorAction SilentlyContinue				# Windows Communicationsapps
+Get-AppxPackage "*WindowsFeedbackHub*"  | Remove-AppxPackage  -ErrorAction SilentlyContinue						# Windows Feedback Hub
+Get-AppxPackage "*windowsmaps*"  | Remove-AppxPackage  -ErrorAction SilentlyContinue							# Windows Maps
+Get-AppxPackage "*windowsphone*"  | Remove-AppxPackage  -ErrorAction SilentlyContinue							# Windows Phone
+Get-AppxPackage "*WindowsScan*"  | Remove-AppxPackage  -ErrorAction SilentlyContinue							# Windows Scan
+Get-AppxPackage "*Xbox.TCUI*"  | Remove-AppxPackage -ErrorAction SilentlyContinue								# XBOX TCUI
+Get-AppxPackage "*XboxGameOverlay*"  | Remove-AppxPackage  -ErrorAction SilentlyContinue						# XBOX GameOverlay
+Get-AppxPackage "*XboxIdentityProvider*"  | Remove-AppxPackage  -ErrorAction SilentlyContinue					# XBOX IdentityProvider
+Get-AppxPackage "*XboxOneSmartGlass*"  | Remove-AppxPackage  -ErrorAction SilentlyContinue						# XBOX One SmartGlass
+Get-AppxPackage "*XboxSpeechToTextOverlay*"  | Remove-AppxPackage  -ErrorAction SilentlyContinue				# XBOX Speech to Text Overlay
+Get-AppxPackage "*YourPhone*"  | Remove-AppxPackage -ErrorAction SilentlyContinue								# YourPhone
+Get-AppxPackage "*zunemusic*"  | Remove-AppxPackage  -ErrorAction SilentlyContinue								# ZuneMusic
+Get-AppxPackage "*zunevideo*"  | Remove-AppxPackage  -ErrorAction SilentlyContinue								# ZuneVideo
+Get-AppxPackage "*CloudExperienceHost*"  | Remove-AppxPackage -ErrorAction SilentlyContinue						# Cloud ExperienceHost
+Get-AppxPackage "*HEIFImageExtension*"  | Remove-AppxPackage -ErrorAction SilentlyContinue						# HEIFImageExtension
+Get-AppxPackage "*MixedReality.Portal*"  | Remove-AppxPackage -ErrorAction SilentlyContinue						# Mixed Reality Portal
+Get-AppxPackage "*print3d*"  | Remove-AppxPackage -ErrorAction SilentlyContinue									# Print 3D
+Get-AppxPackage "*ScreenSketch*"  | Remove-AppxPackage -ErrorAction SilentlyContinue							# ScreenSketch (Skizzieren)
+Get-AppxPackage "*WebMediaExtensions*"  | Remove-AppxPackage -ErrorAction SilentlyContinue						# WebMediaExtensions
+Get-AppxPackage "*WebpImageExtension*"  | Remove-AppxPackage -ErrorAction SilentlyContinue						# WebpImageExtension
+Get-AppxPackage "*XboxGameCallableUI*"  | Remove-AppxPackage -ErrorAction SilentlyContinue						# XBOX Game CallableUI
+Get-AppxPackage "*NetworkSpeedTest*" | Remove-AppxPackage -ErrorAction SilentlyContinue							# Microsoft Networt Speed Test
+Get-AppxPackage "*Spielleiste*" | Remove-AppxPackage -ErrorAction SilentlyContinue								# Microsoft Spielleiste (gehört zu XBOX)
+Get-AppxPackage "*OfficeLens*" | Remove-AppxPackage -ErrorAction SilentlyContinue								# Office Lens
+
+taskkill /f /im OneDrive.exe
+& "$env:SystemRoot\SysWOW64\OneDriveSetup.exe" /uninstall
